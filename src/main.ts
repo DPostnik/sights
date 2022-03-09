734f912f57301325a4ab6042a59327e2f47a8ee2
daniil.postnik <daniil.postnik@intexsoft.by>
2022-03-09 23:52:24 +0300

    added cors middleware, update token

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ declare const module: any;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   console.log('Server is running on port ' + process.env.PORT);
-
+  app.enableCors();
   const config = new DocumentBuilder()
     .setTitle('Sights of Belarus')
     .setDescription('Implementation')
