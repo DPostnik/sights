5b3f2b5167f86b3af03bac651004ae766fbb3009
DPostnik <d.postnik1@gmail.com>
2022-03-10 22:24:32 +0300

    added OAuth2.0 via Google authorization, implemented GoogleStrategy

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -58,4 +58,15 @@ export class AuthService {
       message: 'Некорректный логин или пароль',
     });
   }
+
+  googleLogin(req) {
+    if (!req.user) {
+      return 'No user from google';
+    }
+
+    return {
+      message: 'User information from google',
+      user: req.user,
+    };
+  }
 }
